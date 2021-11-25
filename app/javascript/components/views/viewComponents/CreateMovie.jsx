da09f46c63c1fe2840d24f6360fb73c221e6abfb
Martin Ruiz-Tagle <martinrtc@uc.cl>
2021-11-25 16:55:06 -0300

    avances vista crear pelicula

--- app/javascript/components/views/viewComponents/CreateMovie.jsx
+++ app/javascript/components/views/viewComponents/CreateMovie.jsx
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, ListItem, List, Typography } from '@mui/material'
+import { Box, Typography, TextField, Select } from '@mui/material'
+
+
 
 export default function CreateMovie() {
+    const [movieName, setMovieName] = useState('')
+
+    const handleChange = (event) => {
+        setMovieName(event.target.value);
+        console.log(movieName)
+    };
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Typography>Crear película</Typography>
+            <TextField
+                required
+                id="outlined-required"
+                label="Nombre"
+                value={movieName}
+                onChange={handleChange}
+                helperText="Eliga el nombre de la película"
+                size="small"
+            />
         </Box>
     );
 }
