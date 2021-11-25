0a9c00dbf744f65d1b04cb42ae849681bdcfe259
Martin Ruiz-Tagle <martinrtc@uc.cl>
2021-11-25 16:29:41 -0300

    avances en vistas de reservacion

---
+++ app/javascript/components/views/viewComponents/CreateMovie.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button, ListItem, List, Typography } from '@mui/material'
+
+export default function CreateMovie() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Typography>Crear película</Typography>
+        </Box>
+    );
+}
