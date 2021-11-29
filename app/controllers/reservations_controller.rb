3725f4ef8f975da322bbf9fe441833ae9a4868ab
slecaros <slecaros@uc.cl>
2021-11-29 03:46:50 -0300

    fix double seat

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -34,11 +34,15 @@ class ReservationsController < ApplicationController
       seats: params[:seats],
       user_id: user.id,
     )
-    Reservation.check_seats(params[:schedule_id], params[:date], params[:row], params[:seats])
-    if @reservation.save
-      render :show, status: :created, location: @reservation
-    else
-      render json: @reservation.errors, status: :unprocessable_entity
+    begin
+      Reservation.check_seats(params[:schedule_id], params[:date], params[:row], params[:seats])
+      if @reservation.save
+        render :show, status: :created, location: @reservation
+      else
+        render json: @reservation.errors, status: :unprocessable_entity
+      end
+    rescue => exception
+      json_response({}, 403)
     end
   end
 
